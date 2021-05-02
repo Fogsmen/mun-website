8e9e4cb3893d0a885098b6706670e2ed60f1a0de
unknown <ynarula678@gmail.com>
2021-05-02 13:24:19 +0530

    preloader

--- app.js
+++ app.js
@@ -13,3 +13,14 @@ function SubForm (){
     });
 }
 //loader
+var preload = document.createElement("div");
+preload.className = "preloader";
+preload.innerHTML =
+  '<div id="preloader"><div id="loader"></div></div>';
+document.body.appendChild(preload);
+window.addEventListener("load", function() {
+  preload.className += " fade";
+  setTimeout(function() {
+    preload.style.display = "none";
+  }, 5000);
+});
